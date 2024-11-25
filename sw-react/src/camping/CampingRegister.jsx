5e77a8ed96d447c9ec6bc5339d844f22d6eed92f
asdop0 <dksalstjr2@gmail.com>
2024-11-25 15:32:07 +0900

    캠핑장, 게시글 등록 기능 추가

--- sw-react/src/camping/CampingRegister.jsx
+++ sw-react/src/camping/CampingRegister.jsx
@@ -1,18 +1,89 @@
-import React from "react";
-const CampingRegister = (onClose) => {
+import React, { useState } from "react";
+import SignApiClient from "../services/auth/SignApiClient";
+import AdminApiClient from "../services/camping/AdminApiClient";
+import { useNavigate } from 'react-router-dom';
+
+const CampingRegister = ( onClose ) => {
+  // 각 입력값을 상태로 관리
+  const [name, setName] = useState('');
+  const [homepage, setHomepage] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  const [phonenumber, setPhonenumber] = useState('');
+  const [address, setAddress] = useState('');
+  const [district, setDistrict] = useState('');
+  const navigate = useNavigate(); // useNavigate 사용
+
+  const handleAddCamping = () => {
+    SignApiClient.loginCheck();
+    const accessToken = localStorage.getItem("accessToken"); // 토큰 저장 위치 확인 필요
+    AdminApiClient.addCamping(accessToken, name, address, district, homepage, latitude, longitude, phonenumber)
+      .then((res) => {
+        if (res.ok) {
+          alert("등록되었습니다.");
+          navigate('/');
+        } else {
+          console.error("Failed to add camping.");
+        }
+      })
+      .catch((err) => console.error(err));
+  };
+
 
   return (
     <div className="camping_register">
-        <h2>캠핑장 등록</h2>
-        <input className="camping_register_name" placeholder="캠핑장 이름" type="camping_name" /> 
-        <input className="camping_register_homepage" placeholder="홈페이지" type="homepage" /><br/>
-        <input className="camping_register_Latitude" placeholder="위도" type="Latitude" />
-        <input className="camping_register_longitude" placeholder="경도" type="longitude" />
-        <input className="camping_register_Coordinates" placeholder="좌표" type="Coordinates" /><br/>
-        <input className="camping_register_address" placeholder="주소" type="address" />
-        <input className="camping_register_disctrict" placeholder="지역구분" type="disctrict" /><br/>
-        <button >확인</button>
-        <button onClick={onClose}>닫기</button>
+      <h2>캠핑장 등록</h2>
+      <input
+        className="camping_register_name"
+        placeholder="캠핑장 이름"
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        className="camping_register_homepage"
+        placeholder="홈페이지"
+        type="text"
+        value={homepage}
+        onChange={(e) => setHomepage(e.target.value)}
+      /><br/>
+      <input
+        className="camping_register_Latitude"
+        placeholder="위도"
+        type="text"
+        value={latitude}
+        onChange={(e) => setLatitude(e.target.value)}
+      />
+      <input
+        className="camping_register_longitude"
+        placeholder="경도"
+        type="text"
+        value={longitude}
+        onChange={(e) => setLongitude(e.target.value)}
+      />
+      <input
+        className="camping_register_Phonenumber"
+        placeholder="전화번호"
+        type="text"
+        value={phonenumber}
+        onChange={(e) => setPhonenumber(e.target.value)}
+      /><br/>
+      <input
+        className="camping_register_address"
+        placeholder="주소"
+        type="text"
+        value={address}
+        onChange={(e) => setAddress(e.target.value)}
+      />
+      <input
+        className="camping_register_disctrict"
+        placeholder="지역구분"
+        type="text"
+        value={district}
+        onChange={(e) => setDistrict(e.target.value)}
+      /><br/>
+      <button onClick={handleAddCamping}>확인</button>
+      <button onClick={onClose}>닫기</button>
     </div>
   );
 };
--- sw-react/src/used_store/Used_store.jsx
+++ sw-react/src/used_store/Used_store.jsx
@@ -2,17 +2,20 @@ import React, { useEffect, useState } from "react";
 import "./Used_store.css";
 import UsedCard from "./UsedCard"; // UsedCard 컴포넌트를 가져옵니다.
 import BoardApiClient from "../services/board/BoardApiClient";
+import { Link } from 'react-router-dom';
 
 const Used_store = () => {
   const [boards, setBoards] = useState([]);
   const [sortBy, setSortBy] = useState("최신");
   const [pageRefresh, setPageRefresh] = useState(true);
+  const [role, setRole] = useState(null);
 
   const handleSortByChange = (event) => {
     setSortBy(event.target.value);
   };
 
   useEffect(() => {
+    setRole(localStorage.getItem("role"));
     if(sortBy === "최신") {
       BoardApiClient.getBoardList().then(res => {
         if(res.ok) {
@@ -71,12 +74,20 @@ const Used_store = () => {
       <option value="조회순">조회순</option>
       <option value="댓글순">댓글순</option>
     </select>
-    <button className="board_register_button">등록</button>
+    {((role === 'ROLE_ADMIN') || (role === 'ROLE_USER')) && (<Link to={`/board/register`}>
+         <button className='board_plus'>
+          <img 
+            className='plus'
+            src="/plus.png" 
+            alt="등록 버튼"
+          />
+          </button>
+          </Link>)}
       <div className="board_list">
         {boards.length > 0 ? (
           boards.map((board) => (
             <div key={board.id} className="board_card_wrapper">
-              <UsedCard board={board} /> {/* 각 제품에 대한 UsedCard 컴포넌트를 사용 */}
+              <UsedCard board={board} setPageRefresh={setPageRefresh}/> {/* 각 제품에 대한 UsedCard 컴포넌트를 사용 */}
             </div>
           ))
         ) : (
